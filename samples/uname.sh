261bd2fe045e1b2fbb1ccbc24f8da231b33c7f45
Teja <tejachelamala123@gmail.com>
2026-02-16 16:44:13 +0530

    message

--- samples/uname.sh
+++ samples/uname.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo "Please enter your username::"
-read USERNAME
-echo "Entered username is $USERNAME"
+echo "Please enter your username1::"
+read USERNAME1
+
+echo "please enter your password1::"
+read PASSWORD1
+
+echo "Entered username1 is $USERNAME1"
+echo "Enetred password1 is $PASSWORD1"
+
+echo "Please enter your username2::"
+read USERNAME2
+echo "Please enter your password2"
+read -s PASSWORD2
+
+echo "Entered Username2 is:: ${USERNAME2}"
+echo "Enetered password2 is:: $PASSWORD2"
+
