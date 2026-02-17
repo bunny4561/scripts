8df180c918b705b7e6205f48fdff8c6db1d92015
Teja <tejachelamala123@gmail.com>
2026-02-17 11:31:46 +0530

    message

---
+++ samples/special-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All variables passed:: $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Present working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo " Which user is running the sctript: $USER"
+echo "processes id of current running script: $$"
+echo "processed id of last command running in background: $!"
