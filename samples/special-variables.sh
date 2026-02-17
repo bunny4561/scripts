925c8310128e56ebddaee6c41abf40341a5beba4
Teja <tejachelamala123@gmail.com>
2026-02-17 11:46:55 +0530

    message

--- samples/special-variables.sh
+++ samples/special-variables.sh
@@ -7,5 +7,5 @@ echo "Present working directory: $PWD"
 echo "Home directory of current user: $HOME"
 echo " Which user is running the sctript: $USER"
 echo "processes id of current running script: $$"
-sleep 30
+sleep 30 &
 echo "processed id of last command running in background: $!"
