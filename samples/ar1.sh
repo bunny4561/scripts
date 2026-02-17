bce8640a7eab26aadea9407ec293037970161893
Teja <tejachelamala123@gmail.com>
2026-02-17 10:20:11 +0530

    message

--- samples/ar1.sh
+++ samples/ar1.sh
@@ -4,4 +4,6 @@ MOVIES=("Ramnagstalam" "RRR" "Pushpa")
 
 echo " Entered first movie is ${MOVIES[0]}"
 echo "Entered second movie is ${MOVIES[1]}"
-echo Entered third movie is ${MOVIES[2]}"
+echo "Entered third movie is ${MOVIES[2]}"
+
+echo "Entered all movies are ${MOVIES[@]}"
