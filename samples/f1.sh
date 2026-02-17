839eae9eb40ed9daf394b45f4a45233b224a59e9
Teja <tejachelamala123@gmail.com>
2026-02-17 12:51:06 +0530

    message

--- samples/f1.sh
+++ samples/f1.sh
@@ -6,5 +6,9 @@ if [ $NUM -gt 100 ]
 then
     echo "Entered number $NUM is greater than 100"
 else
+    if [ $NUM -eq 100 ]
+    then
+        echo "Entered number $NUM is equals to 100"
+    else
     echo " Entered number $NUM is less than or equals to 100"
 fi
