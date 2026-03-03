93e2b1daa61912bb79aa5d3ac4aeae5199f005ba
Teja <tejachelamala123@gmail.com>
2026-03-03 15:07:08 +0530

    Sample Function script added

--- samples/f4.sh
+++ samples/f4.sh
@@ -26,21 +26,21 @@ VALIDATE()
     fi
 }
 
-echo "Script started executing at $TIMESTAMP" &>> $LOG_FILE_NAME  
+echo "Script started executing at $TIMESTAMP" &>>$LOG_FILE_NAME  
 
-dnf list installed mysql &>> $LOG_FILE_NAME
+dnf list installed mysql &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install mysql -y &>> $LOG_FILE_NAME
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "MySQL installation"
 else
     echo -e "MySQl is already $Y installed"
 fi
 
-dnf list installed git &>> $LOG_FILE_NAME
+dnf list installed git &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install git -y &>> $LOG_FILE_NAME
+    dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Git installation"
 else
     echo -e "Git is already $Y installed"
