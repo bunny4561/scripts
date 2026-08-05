f4b8b0e5e3cd907ebbf182ed16ec2294907148b3
Teja <tejachelamala123@gmail.com>
2026-08-05 19:30:44 +0530

    conversation added

--- samp/convo.sh
+++ samp/convo.sh
@@ -16,3 +16,5 @@ P2=$2
 echo "$P1:: Hi $P2!"
 echo "$P2:: Hello $P1!"
 echo "It was very got to add these convo with both $P1 and $P2!"
+
+echo "The final verdit of these all good persons are as : Rahim , Rahul , $PERSON1 , $PERSON2 , $P1 , $P2!"
