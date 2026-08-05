918baa6c571b95d4cc2f8543b306111e54e6bc94
Teja <tejachelamala123@gmail.com>
2026-08-05 19:29:08 +0530

    conversation added

--- samp/convo.sh
+++ samp/convo.sh
@@ -9,3 +9,10 @@ PERSON2=Kishore
 echo "$PERSON1:: Hi $PERSON2!"
 echo "$PERSON2:: Hello $PERSON1!"
 echo "My friends are both $PERSON1 and $PERSON2"
+
+P1=$1
+P2=$2
+
+echo "$P1:: Hi $P2!"
+echo "$P2:: Hello $P1!"
+echo "It was very got to add these convo with both $P1 and $P2!"
