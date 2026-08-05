03d3e26aef3807e234d746c982fd7f935c6c599c
Teja <tejachelamala123@gmail.com>
2026-08-05 19:24:19 +0530

    conversation added

--- samp/convo.sh
+++ samp/convo.sh
@@ -2,3 +2,10 @@
 
 echo "Rahim:: Hi Rahul!"
 echo "Rahul:: Hello Rahim!"
+
+PERSON1=Kiran
+PERSON2=Kishore
+
+echo "$PERSON1:: Hi $PERSON2!"
+echo "$PERSON2:: Hello $PERSON1!"
+echo "My friends are both $PERSON1 and $PERSON2"
