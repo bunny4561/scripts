386908c5849e9a3ca213004e28958b0ca04bb478
Teja <tejachelamala123@gmail.com>
2026-08-05 20:12:21 +0530

    conversation added

---
+++ samp/usrname.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Please Enter your Username below: "
+read USERNAME #here it is variable
+echo "Entered username is :: $USERNAME"
+echo "Please enter your password below: "
+read -s PASSWORD
+echo "Entered password is $PASSWORD"
