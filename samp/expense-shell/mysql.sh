a86eecd5a4828a5b3ba13ff2dac5dcadabbb2908
Teja <tejachelamala123@gmail.com>
2026-09-03 15:55:18 +0530

    conversation added

--- samp/expense-shell/mysql.sh
+++ samp/expense-shell/mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/espense_shell_logs"
+LOGS_FOLDER="/var/log/expense_shell_logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -27,24 +27,28 @@ CHECK_ROOT() {
 
 CHECK_ROOT
 
-echo "Script executed at $TIMESTAMP"
+echo "Script executed at $TIMESTAMP" &>>$LOG_FILE_NAME
 
-dnf install mysql-server -y
+mkdir -p /var/logs/expense_shell_logs &>>$LOG_FILE_NAME
+
+dnf install mysql-server -y &>>$LOG_FILE_NAME
 VALIDATE $? "Installing mysql-server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Enabling mysqld service"
 
-systemctl start mysqld
+systemctl start mysqld &>>$LOG_FILE_NAME
 VALIDATE $? "Starting mysqld service"
 
-mysql -h 172.31.20.69 -u root -pExpenseApp@1 -e 'show databases;
+mysql -h 172.31.20.69 -u root -pExpenseApp@1 -e 'show databases; &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
     echo "MySQL Root password not setup" 
-    mysql_secure_installation --set-root-pass ExpenseApp@1
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOG_FILE_NAME
     VALIDATE $? "Setting Root Password"
 else
     echo "MySQL Root password already setup ...SKIPPING"
 fi
+
+echo "Script ended at $TIMESTAMP" &>>$LOG_FILE_NAME
